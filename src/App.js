3ef75133590155e0aa774586ca8aa699455e3192
Anderson <andersonst.dev@gmail.com>
2020-04-10 14:21:05 -0300

    config loaders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+
+import './App.css';
+import backgroundImage from './assets/city.jpg';
+
+/**
+ * Component;
+ * Propriedade -> Informações que pode ser passadas de um component pai 
+ * para um component filho
+ * Estado & imutabilidade -> Imutabilidade é não mutar a variavel. Ela deve ser
+ * recriada do zero com a nova informação adicionada. Estado serve para armazenar dados.
+ */
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [projects, setProjects] = useState(
+    ['Desenvolvimento de App', 'Front-end Web']);
+
+  function handleAddProject() {
+    setProjects(currentProjects => 
+      [...currentProjects, `Novo projeto ${Date.now()}`]);
+  } 
+
+  return (
+    <>  
+      <Header title="Projects" />
+
+      <img src={backgroundImage} alt="imagem"/>
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+    </>
+  );
 }
 
 export default App;
