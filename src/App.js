e49d04d684b0779d88de981152aba7a218fcc7b3
Anderson <andersonst.dev@gmail.com>
2020-04-10 18:09:00 -0300

    connects to the api using axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import Header from './components/Header';
 
 import './App.css';
-import backgroundImage from './assets/city.jpg';
 
-/**
- * Component;
- * Propriedade -> Informações que pode ser passadas de um component pai 
- * para um component filho
- * Estado & imutabilidade -> Imutabilidade é não mutar a variavel. Ela deve ser
- * recriada do zero com a nova informação adicionada. Estado serve para armazenar dados.
- */
+
 
 function App() {
-  const [projects, setProjects] = useState(
-    ['Desenvolvimento de App', 'Front-end Web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(res => {
+      setProjects(res.data);
+    })
+  }, []);
 
   function handleAddProject() {
     setProjects(currentProjects => 
@@ -26,10 +25,8 @@ function App() {
     <>  
       <Header title="Projects" />
 
-      <img src={backgroundImage} alt="imagem"/>
-
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>
